e6e08af997cbd631edef5ce5b18005da9644f77a
Claudia Glemser <claudia.glemser@student.uni-tuebingen.de>
2020-05-30 12:57:32 +0200

    added masterFile and descriptive Stats script

---
+++ 1_code/0_masterFile.R
@@ -0,0 +1,68 @@
+###############################################################################
+##============================= 0_masterFile.R ==============================##
+###############################################################################
+
+# once you've cloned the repository:
+#  1) set your working directory to the folder "Zempeltzi_etal_2020/1_code"
+#  2) choose which analyses you want to run (a few lines down)
+#  3) run this file
+
+## load necessary libraries ##
+library(ggplot2)
+
+load("../0_data/Dataset.RData")
+
+ori_data <- gesamt_data  # safety copy of original dataset 
+
+# the paper only looks at the first two phases
+gesamt_data <- gesamt_data[gesamt_data$phase %in% c("detect01", "discr01"),]
+
+## TODO:
+# include this in ReadingIn-File -> Dataset only includes phase 1 and 2
+
+mainDirPlots <- paste0("../3_output/figures/")
+mainDirCSV   <- paste0("../3_output/statistics/")
+
+###=== CHOOSE which analyses you want to run ===###
+
+# corresponds to Figure 1
+runDescriptives <- TRUE
+
+# corresponds to Figure 2 and 3
+runANOVAs <- TRUE
+
+# corresponds to Figure 4, 5 and 6
+runGLMMs <- TRUE
+
+# corresponds to Figure 7
+runTemporalGLMMs <- TRUE
+runAllGLMMs <- FALSE
+# for convenience the output csv files needed to plot the temporal resolution
+#   of the GLMMs is already provided in the folder 2_pipeline! 
+# you can also choose to rerun the GLMMs for all bins and variables.
+#   be however aware that this will take a good amount of time to run.
+
+###=== CALLS all requested analysis files ===###
+if(runDescriptives){
+  source("1_descriptiveStats.R")
+}
+
+if(runANOVAs){
+#  source("2_ANOVAs.R")
+}
+
+if(runGLMMs){
+#  source("3_GLMMs_1binBeforeReaction.R
+}
+
+if(runTemporalGLMMs){
+# put this in pipeline:
+#  if(rerunAllGLMMs){
+#    source("4_GLMMs_2to4binBeforeReaction.R")  # this sd be in pipeline?
+#  }
+#  source("5_GLMMs_tempResolution.R")
+}
+
+# The output figures and stats tables will be in the correspondingly
+#   numbered subfolders in 3_output!
+
---
+++ 1_code/1_descriptiveStats.R
@@ -0,0 +1,57 @@
+###############################################################################
+##========================= 1_descriptiveStats.R ============================##
+###############################################################################
+
+# plots the reaction time histograms for the detection and discrimination
+# phase
+#
+# input: gesamt_data from 0_masterFile.R
+# output_ Figure1c_detect.pdf, Figure1c_discrimination.pdf
+
+# exclude all reaction times above 6 seconds
+gesamt_data_rt_smaller_6 <- subset(gesamt_data, rt <= 6)
+
+## barchart for detection phase
+temp_data <- subset(gesamt_data_rt_smaller_6, phase == "detect01")
+
+# sort data into bins according to reaction time  
+temp_data$RTbin <- findInterval(temp_data$rt, c(seq(0, 6, length.out = 40)))
+
+# plot barchart()
+p1 <- ggplot(temp_data, aes(x = RTbin, fill = answer)) +
+      geom_bar() + 
+      scale_x_continuous(breaks = seq(1, 40, length.out = 13),
+                         labels = seq(0,6,0.5),
+                         expand = expand_scale(add = c(1, 1))) +
+      facet_wrap(~phase) +  # facet_wrap to make it prettier: phase on top
+      geom_vline(xintercept = seq(1, 40, length.out = 5), col = "red") +
+      theme_bw()
+   
+# save as pdf        
+file_name <-  paste0(mainDirPlots, "Figure1c_detect.pdf")
+pdf(file = file_name, width = 10, height = 8)  
+print(p1)
+dev.off()
+     
+
+## barchart for detection phase
+temp_data <- subset(gesamt_data_rt_smaller_6, phase == "discr01")
+
+# sort data into bins according to reaction time  
+temp_data$RTbin <- findInterval(temp_data$rt, c(seq(0, 6, length.out = 40)))
+
+# plot barchart()
+p1 <- ggplot(temp_data, aes(x = RTbin, fill = answer)) +
+      geom_bar() + 
+      scale_x_continuous(breaks = seq(1, 40, length.out = 13),
+                         labels = seq(0,6,0.5),
+                         expand = expand_scale(add = c(1, 1))) +
+      facet_wrap(~phase) +  # facet_wrap to make it prettier: phase on top
+      geom_vline(xintercept = seq(1, 40, length.out = 5), col = "red") +
+      theme_bw()
+      
+# save as pdf     
+file_name <-  paste0(mainDirPlots, "Figure1c_discrimination.pdf")
+pdf(file = file_name, width = 10, height = 8)  
+print(p1)
+dev.off()
